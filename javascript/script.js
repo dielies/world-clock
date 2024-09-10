4ad8f6a43bac848a54e0b6672e66d5f7605e21d5
dielies <160782509+dielies@users.noreply.github.com>
2024-09-10 21:27:33 +0200

    added dropdown functionality timezones

--- javascript/script.js
+++ javascript/script.js
@@ -1,23 +1,51 @@
 function updateTime() {
   let aucklandElement = document.querySelector("#auckland");
-  let aucklandDateElement = aucklandElement.querySelector(".date");
-  let aucklandTimeElement = aucklandElement.querySelector(".time");
-  let aucklandTime = moment().tz("Pacific/Auckland");
+  if (aucklandElement) {
+    let aucklandDateElement = aucklandElement.querySelector(".date");
+    let aucklandTimeElement = aucklandElement.querySelector(".time");
+    let aucklandTime = moment().tz("Pacific/Auckland");
 
-  aucklandDateElement.innerHTML = aucklandTime.format("MMMM Do YYYY");
-  aucklandTimeElement.innerHTML = aucklandTime.format(
-    "H:mm:ss[<small>]A[</small>]"
-  );
+    aucklandDateElement.innerHTML = aucklandTime.format("MMMM Do YYYY");
+    aucklandTimeElement.innerHTML = aucklandTime.format(
+      "H:mm:ss[<small>]A[</small>]"
+    );
+  }
 
   let saoPauloElement = document.querySelector("#sao-paulo");
-  let saoPauloDateElement = saoPauloElement.querySelector(".date");
-  let saoPauloTimeElement = saoPauloElement.querySelector(".time");
-  let saoPauloTime = moment().tz("America/Sao_Paulo");
+  if (saoPauloElement) {
+    let saoPauloDateElement = saoPauloElement.querySelector(".date");
+    let saoPauloTimeElement = saoPauloElement.querySelector(".time");
+    let saoPauloTime = moment().tz("America/Sao_Paulo");
 
-  saoPauloDateElement.innerHTML = saoPauloTime.format("MMMM Do YYYY");
-  saoPauloTimeElement.innerHTML = saoPauloTime.format(
-    "H:mm:ss[<small>]A[</small>]"
-  );
+    saoPauloDateElement.innerHTML = saoPauloTime.format("MMMM Do YYYY");
+    saoPauloTimeElement.innerHTML = saoPauloTime.format(
+      "H:mm:ss[<small>]A[</small>]"
+    );
+  }
 }
 updateTime();
 setInterval(updateTime, 1000);
+
+function updateCity(event) {
+  let cityTimeZone = event.target.value;
+  let cityName = cityTimeZone.replace("_", " ").split("/")[1];
+  let cityTime = moment().tz(cityTimeZone);
+  let citiesElement = document.querySelector("#cities");
+  citiesElement.innerHTML = `
+      <div class="city">
+                <div>
+                    <h2>
+                    ${cityName}
+                    </h2>
+                    <div class="date">${cityTime.format("MMMM Do YYYY")}</div>
+                </div>
+                <div class="time">${cityTime.format(
+                  "H:mm:ss"
+                )}<small>${cityTime.format("A")}</small>
+                </div>
+            </div>
+            `;
+}
+
+let citiesSelectElement = document.querySelector("#city");
+citiesSelectElement.addEventListener("change", updateCity);
